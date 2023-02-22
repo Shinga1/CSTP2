1a7d6ea8b7d47a092d4895109275906b04a69495
Ismail Hussain <210079573@aston.ac.uk>
2023-02-22 23:15:56 +0000

    added products to database

--- mySQL database/celessentials.sql
+++ mySQL database/celessentials.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2023 at 10:40 PM
+-- Generation Time: Feb 23, 2023 at 12:12 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,75 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `celessentials`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `personal_access_tokens`
---
-
-CREATE TABLE `personal_access_tokens` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `last_used_at` timestamp NULL DEFAULT NULL,
-  `expires_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -114,56 +45,26 @@ INSERT INTO `products` (`product_id`, `product_name`, `product_image`, `product_
 (1, 'Fenty Beauty Lipstick', 'fenty.jpg', 'The semi-matte, creamy formula hugs lips with a smooth, plush texture and long lasting iconic wear.', 20, 'Beauty', 10),
 (2, 'Beats by Dr Dre', 'beats.jpg', 'Beats by Dr. Dre (Beats) is a leading audio brand founded in 2006 by Dr. Dre and Jimmy Iovine.', 350, 'Electronics', 10),
 (3, 'Snoop Cereal by Snoop Dogg', 'snoop-loopz.jpg', 'This cereal is gluten-free, sugary, colorful, and full of marshmallows and produced by Broadus Foods, Snoops family-owned food product company.', 20, 'Food', 10),
-(4, 'Yeezy by Kanye West', 'yeezy.jpg', 'Yeezy was a fashion collaboration between American rapper, designer and entrepreneur Kanye West and German sportswear company Adidas.', 200, 'Perfumes', 10),
-(5, 'Book', 'book.jpg', 'book description', 0, 'Books', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(4, 'Dior Sauvage', 'sauvage.jpg', 'Sauvage Eau de Toilette, 60 ml. Fresh, citrusy and woody, the scent of this singular eau de toilette conveys the spirit of wide-open spaces.', 110, 'Perfumes', 10),
+(5, 'David Beckham: My Side', 'beckham.jpg', 'This is David Beckham\'s own story of his career to date, for Manchester United, Real Madrid and England, and of his childhood, family and private life.', 10, 'Books', 5),
+(6, 'Rare Beauty Under Eye Brightener', 'rare.jpg', 'A super lightweight liquid that instantly brightens, hydrates, and awakens undereyes with sheer to medium coverage for a fresh, radiant look in a flash.', 26, 'Beauty', 20),
+(7, 'Rhode Glazing Fluid', 'rhode.jpg', 'Hailey’s signature step to dewy, glazed skin. A lightweight, quick-absorbing, gel serum that visibly plumps and hydrates to support a healthy skin barrier. Size: 50ml / 1.7oz.', 29, 'Beauty', 100),
+(8, 'r.e.m lip liner pencil', 'rem.jpg', 'Create fuller looking lips with this highly pigment liner pencil that pairs perfectly with our matte liquid lipsticks and classic cream lipsticks. it’s formulated with next gen waxes with a creamy feel that glides onto lips. line, define—and watch ‘em line up.', 17, 'Beauty', 15),
+(9, 'Haus Labs Atmic Shake Lip Laqcuer', 'gaga.jpg', 'Paint on high impact liquid lipstick with a transfer-proof, super glossy finish that lasts and lasts. No reapplying necessary.\r\n\r\nHausTech Powered™ with Marine Algae + Shine Boos', 27, 'Beauty', 30),
+(10, 'Will (Hardback)', 'will.jpg', 'Tracing the incredible career of one of the biggest music and film stars of all time, Will Smith\'s inspirational memoir also homes in on the emotional toll that stardom can exact and provides sound advice to all those who think they have mastered life only to find themselves unfulfilled.', 20, 'Books', 20),
+(11, 'Open Book (Hardcover )', 'Jessica.jpg', 'Jessica reveals for the first time her inner monologue and most intimate struggles. Guided by the journals she\'s kept since age fifteen, and brimming with her unique humour and down-to-earth humanity, Open Book is as inspiring as it is entertaining.', 17, 'Books', 20),
+(12, 'Undisputed Truth: My Autobiography Paperback', 'tyson.jpg', 'One of the most talked-about and bestselling books of last year, this is the no-holds-barred autobiography of a sporting legend driven to the brink of self-destruction', 10, 'Books', 0),
+(13, 'Unfinished (Hardcover)', 'chopra.jpg', 'The thoughtful, revealing and NEW YORK TIMES BESTSELLING memoir from one of the world\'s most recognisable women, renowned for her bold risk-taking and activism', 16, 'Books', 20),
+(14, 'TRDR by Souija Boy', 'trdr.jpg', '“A disappointing handheld with little innovation, poor marketing and a bunch of problems”', 400, 'Electronics', 15),
+(15, 'Will I Am Puls Smart Watch', 'puls.jpg', 'The i.am PULS (by Black Eyed Peas member and founder Will.i.am) is a smart cuff designed to replace your phone entirely. It features a 1.7-inch touchscreen display, which can be used to make calls, send texts, respond to emails, and more. ', 300, 'Electronics', 0),
+(16, 'Virginia Black Whiskey by Drake', 'drake.jpg', 'Virginia Black Decadent American Whiskey. A collaboration between world renowned, platinum selling recording artist Drake and luxury spirits veteran Brent Hocking; the only three-time winner of the illustrious “Spirit of the Year” award.', 45, 'Foods', 30),
+(17, 'Aviation American Gin by Ryan Reynolds', 'ryan.jpg', 'Aviation Gin is a completely unique and distinguished gin from Portland, Oregon. Based on a ‘Botanical Democracy’ Aviation Gin has a balance of flavours rather than being dictated by juniper.', 25, 'Foods', 20),
+(18, 'Casamigos Blanco Tequila by George Clooney', 'clooney.jpg', 'Casamigos Blanco Tequila is a small batch, ultra-premium 100 Percent Blue Weber agave tequila founded by George Clooney and Rande Gerber', 52, 'Foods', 50),
+(19, 'Antonio Banderas Perfumes - The Secret - Eau ', 'antonio.jpg', 'Pure magnetism. The Secret by Antonio Banderas, a mysterious and magnetic men\'s fragrance that holds the true seducer\'s secret to success. A rich personality captured in a sensual, refined and extremely seductive fragrance', 23, 'Perfumes', 30);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indexes for table `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD PRIMARY KEY (`email`);
-
---
--- Indexes for table `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
 -- Indexes for table `products`
@@ -172,45 +73,14 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`);
 
 --
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `personal_access_tokens`
---
-ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
