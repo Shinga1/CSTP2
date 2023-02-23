35b9ebe61093d63a09c8fbcaad767539b11a3638
KS <91375185+ShouldKnowB@users.noreply.github.com>
2023-02-23 23:58:16 +0000

    Minor Changes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,40 +11,54 @@ window.onscroll = function () {
     }
 };
 
-// Previous and Next Buttons for Reviews Section
-const prevButton = document.querySelector(".prev-button");
-const nextButton = document.querySelector(".next-button");
+// Scroller For The Reviews
 const reviews = document.querySelectorAll(".review");
+const body = document.querySelector("body");
+const container = document.querySelector(".reviews-container");
 
 let currentIndex = 0;
 let timerId = null;
 
-const showReview = (index) => {
+const showReview = (index, direction) => {
     reviews.forEach((review, i) => {
         if (i === index) {
             review.style.display = "block";
+            review.classList.add("fly-in-right");
         } else {
-            review.style.display = "none";
+            if (direction === "initial" && i !== 0) {
+                review.style.display = "none";
+            } else {
+                review.style.display = "none";
+                review.classList.remove("fly-in-right");
+                review.classList.remove("fly-out-left");
+            }
         }
     });
 };
 
+showReview(currentIndex, "initial");
+
 const nextReview = () => {
-    currentIndex = (currentIndex + 1) % reviews.length;
-    showReview(currentIndex);
+    const nextIndex = (currentIndex + 1) % reviews.length;
+    showReview(nextIndex, "next");
+    currentIndex = nextIndex;
+
+    // Check if the next review text is offscreen to the left
+    const offscreenLeft =
+        reviews[currentIndex].getBoundingClientRect().left < 0;
+    if (offscreenLeft) {
+        // Move the current review to the end of the container
+        container.appendChild(reviews[currentIndex]);
+
+        // Set the currentIndex to the first review text that's not visible
+        for (let i = 0; i < reviews.length; i++) {
+            const rect = reviews[i].getBoundingClientRect();
+            if (rect.left >= 0 && rect.right <= window.innerWidth) {
+                currentIndex = i;
+                break;
+            }
+        }
+    }
 };
 
 timerId = setInterval(nextReview, 5000);
-
-prevButton.addEventListener("click", () => {
-    clearInterval(timerId);
-    currentIndex = (currentIndex - 1 + reviews.length) % reviews.length;
-    showReview(currentIndex);
-    timerId = setInterval(nextReview, 5000);
-});
-
-nextButton.addEventListener("click", () => {
-    clearInterval(timerId);
-    nextReview();
-    timerId = setInterval(nextReview, 5000);
-});
