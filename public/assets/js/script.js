e6b80f0882719341cfe5526d86f9e340b1ee3209
KS <91375185+ShouldKnowB@users.noreply.github.com>
2023-02-24 23:57:21 +0000

    Minor Changes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -12,25 +12,20 @@ window.onscroll = function () {
 };
 
 // Scroller For The Reviews
-const reviews = document.querySelectorAll(".review");
-const body = document.querySelector("body");
+const reviews = document.getElementsByClassName("review");
 const container = document.querySelector(".reviews-container");
 
 let currentIndex = 0;
-let timerId = null;
 
 const showReview = (index, direction) => {
-    reviews.forEach((review, i) => {
+    Array.from(reviews).forEach((review, i) => {
         if (i === index) {
             review.style.display = "block";
-            review.classList.add("fly-in-right");
         } else {
             if (direction === "initial" && i !== 0) {
                 review.style.display = "none";
             } else {
                 review.style.display = "none";
-                review.classList.remove("fly-in-right");
-                review.classList.remove("fly-out-left");
             }
         }
     });
@@ -43,14 +38,11 @@ const nextReview = () => {
     showReview(nextIndex, "next");
     currentIndex = nextIndex;
 
-    // Check if the next review text is offscreen to the left
     const offscreenLeft =
         reviews[currentIndex].getBoundingClientRect().left < 0;
     if (offscreenLeft) {
-        // Move the current review to the end of the container
         container.appendChild(reviews[currentIndex]);
 
-        // Set the currentIndex to the first review text that's not visible
         for (let i = 0; i < reviews.length; i++) {
             const rect = reviews[i].getBoundingClientRect();
             if (rect.left >= 0 && rect.right <= window.innerWidth) {
@@ -61,4 +53,4 @@ const nextReview = () => {
     }
 };
 
-timerId = setInterval(nextReview, 5000);
+setInterval(nextReview, 4000);
--- resources/views/layouts/frontend/navbar.blade.php
+++ resources/views/layouts/frontend/navbar.blade.php
@@ -13,9 +13,9 @@
     </ul>
 
     <ul class="right">
+        <li><a href="{{ url('/login') }}">Login</a></li>
         <li><a href="{{ url('/register') }}">Register</a></li>
         <li><a href="{{ asset('/') }}"><i class="fa fa-basket-shopping"></i></a></li>
-        <li><a href="{{ asset('/') }}"><i class="fa fa-user"></i></a></li>
     </ul>
 
 </nav>
