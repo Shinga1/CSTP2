59570839ee6f26f39aa75fc18a5679c0ebbee1d7
ManavChana <116156700+ManavChana@users.noreply.github.com>
2023-02-24 15:57:06 +0000

    login page

---
+++ app/Http/Controllers/LoginC.php
@@ -0,0 +1,29 @@
+<?php
+ 
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+ 
+class LoginC extends Controller
+{
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'email' => 'Incorrect Username or Password!',
+        ])->onlyInput('email');
+
+    }
+
+}
