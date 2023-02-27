28b575c082eedee4f7773733d59f048859cb201d
Ameera Desai <200063368@aston.ac.uk>
2023-02-27 15:10:33 +0000

    Contact us page

--- resources/views/frontend/contact_us.blade.php
+++ resources/views/frontend/contact_us.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.main')
 
-<div class="contact_us">
-    <h1>Contact Us</h1>
-</div>
+<body>
+     <div class="contact_us">
+        <h2>Contact Us</h2>
+        <p>Further Enquires?<br> Send us a message, our team will be in touch as soon as possible!</p>
+    </div>
+       <div class="content">
+        <form>
+            <div class="input-group">
+            <input type="text" id="name" required>
+            <label for="name">Your Name:</label>
+        </div>
+        <div class="input-group">
+            <input type="email" id="email" required>
+            <label for="email">Your Email:</label>
+        </div>
+        <div class="input-group">
+            <input type="text" id="subject" required>
+            <label for="subject">Subject:</label>
+        </div>
+        <div class="input-group">
+            <textarea id="message" rows="10" required></textarea>
+            <label for="message">Your Message:</label>
+       </div>
+       <button type="submit">SUBMIT</button>
+    </form>
+    </div>
+</body>
