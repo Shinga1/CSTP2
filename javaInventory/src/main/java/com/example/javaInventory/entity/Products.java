b85c335ce7e76b481ce6bb43f12ad92400e05905
Hasnain Ali <hasnain7815@gmail.com>
2023-03-11 22:16:50 +0000

    Made the Products entity class

---
+++ javaInventory/src/main/java/com/example/javaInventory/entity/Products.java
@@ -0,0 +1,102 @@
+package com.example.javaInventory.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_image")
+    private String productImage;
+
+    @Column(name = "product_description")
+    private String productDescription;
+
+    @Column(name = "product_price")
+    private String productPrice;
+
+    @Column(name = "product_category")
+    private String productCategory;
+
+    @Column(name = "product_stock")
+    private String productStock;
+
+    public Products(String productName, String productImage, String productDescription, String productPrice, String productCategory, String productStock) {
+        this.productName = productName;
+        this.productImage = productImage;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+        this.productCategory = productCategory;
+        this.productStock = productStock;
+    }
+
+    //default constructor created for entity
+    public Products() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductStock() {
+        return productStock;
+    }
+
+    public void setProductStock(String productStock) {
+        this.productStock = productStock;
+    }
+
+}
