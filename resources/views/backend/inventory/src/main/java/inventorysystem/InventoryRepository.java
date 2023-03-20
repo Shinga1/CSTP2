be05a1b7629e3b36aefa55498dffe1d74eaae476
shinga1 <shingai41@outlook.com>
2023-03-20 11:02:29 +0000

    delete java

--- resources/views/backend/inventory/src/main/java/inventorysystem/InventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.inventory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InventoryRepository extends JpaRepository<InventoryItem, Integer> {
-}
--- resources/views/backend/inventory/src/main/java/inventorysystem/SpringBootmySqlExApplication.java
+++
@@ -1,8 +0,0 @@
-package com.sjprogramming.springmysqlex;
-
-import org.springframework.boot.springApplication;
-
-@springBootApplication
-public static void main(String[] args){
-    springApplication.run(springBootMySqlExApplication.class, args)
-}
