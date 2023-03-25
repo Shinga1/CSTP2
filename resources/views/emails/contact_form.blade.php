6ff3fa1756b4c97b9d54a43a9c6ac481504294b6
Hasnain Ali <hasnain7815@gmail.com>
2023-03-25 22:54:42 +0000

    Email format changed

--- resources/views/emails/contact_form.blade.php
+++ resources/views/emails/contact_form.blade.php
@@ -1,4 +1,16 @@
-<p>Name: {{ $contact->name }}</p>
-<p>Email: {{ $contact->email }}</p>
-<p>Subject: {{ $contact->subject }}</p>
-<p>Message: {{ $contact->message }}</p>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Contact Form Submission</title>
+</head>
+<body style="font-family: Arial, sans-serif; font-size: 14px">
+    <h2>New Contact Form Submission</h2>
+    <hr style="border: none; border-top: 2px solid">
+    <p><strong>Name of sender:</strong> {{ $contact->name }}</p>
+    <p><strong>Email:</strong> {{ $contact->email }}</p>
+    <p><strong>Subject:</strong> {{ $contact->subject }}</p>
+    <p><strong>Message:</strong> {{ $contact->message }}</p>
+    <hr style="border: none; border-top: 2px solid">
+    <p style="font-size: 12px;">This message was sent from the contact form on Celessentials website.</p>
+</body>
+</html>
