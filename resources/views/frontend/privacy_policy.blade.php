e040b6ab57345b48b961a09bab9abb5aab26dd8d
KS <91375185+ShouldKnowB@users.noreply.github.com>
2023-03-27 00:41:56 +0100

    Minor Changes

--- resources/views/frontend/privacy_policy.blade.php
+++ resources/views/frontend/privacy_policy.blade.php
@@ -3,45 +3,57 @@
 <body>
     <section class="privacy_policy">
         <div class="content">
-            <h2>privacy policy</h2>
-            <h3>introduction</h3>
-            <p> Punch values your privacy and has created this Privacy Policy to explain how we collect, use,
-                communicate, disclose, and utilise personal information. This Privacy Statement only applies to this
-                website. We are no longer responsible for the privacy practises of the linked website after you enter
-                it, whether through a link provided by this website or through other means. Our privacy policies are as
-                follows: </p>
+            <h2>Privacy Policy</h2>
+            <h3>Introduction</h3>
+            <p> At Celessentials, we value your privacy and have created this Privacy Policy to explain how we collect,
+                use, communicate, disclose, and utilize personal information. This Privacy Statement only applies to our
+                website. Please note that we are not responsible for the privacy practices of any linked website that
+                you
+                may enter, whether through a link provided by our website or through other means. Our privacy policies
+                are as follows:</p>
             <ul>
-                we shall identify the purposes for which personal information is being collected before or at the time
+                We shall identify the purposes for which personal information is being collected before or at the time
                 of collection.
             </ul>
             <ul>
-                we will collect and use personal information for the reasons indicated by us, as well as for other
+                We will collect and use personal information for the reasons indicated by us, as well as for other
                 compatible purposes.
             </ul>
             <ul>
-                we shall only keep personal information for as long as it is required to achieve those aims.
+                We shall only keep personal information for as long as it is required to achieve those aims.
             </ul>
-            <p> This policy applies to information we collect: </p>
+            <p> This policy applies to information we collect:</p>
             <ul>
-                on our website;
-            </ul>
-            <ul>
-                in email, text, and other electronic messages between you and the website;
+                On our website;
             </ul>
             <ul>
-                when you engage with our advertising and applications on third-party sites and services that include
+                In email, text, and other electronic messages between you and our website;
+            </ul>
+            <ul>
+                When you engage with our advertising and applications on third-party sites and services that include
                 links to this policy.
             </ul>
-            <h3>how we collect the information we have on you</h3>
-            <p> We collect numerous sorts of information from and about our Punch users, including: </p>
+            <h3>How We Collect Information</h3>
+            <p> At Celessentials, we collect various types of information from and about our customers, including:</p>
             <ul>
-                personal information is any information that may be used to identify you, such as your name, postal
-                address, e-mail address, phone number, or any other identifier that can be used to contact you online or
-                offline ("personal information").
+                Personal information, which is any information that may be used to identify you, such as your name,
+                postal address, email address, phone number, or any other identifier that can be used to contact you
+                online or offline.
             </ul>
             <ul>
-                concerning your internet connection, the equipment you use to access our website, and how you use it
+                Information about your internet connection, the equipment you use to access our website, and how you use
+                it.
             </ul>
+            <p> As a company that sells celebrity-related products, our goal at Celessentials is to provide our
+                customers
+                with high-quality products associated with their favourite celebrities. Our website offers a wide range
+                of celebrity-produced or endorsed products, including perfumes, accessories, beauty items, and much
+                more. We believe in the power of celebrity culture to inspire and connect people, and we are committed
+                to
+                providing our customers with the most up-to-date celebrity products. All of our customers receive a
+                smooth purchasing experience, and we are dedicated to ensuring the privacy and security of their
+                personal
+                information.</p>
         </div>
     </section>
 </body>
