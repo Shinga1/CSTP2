88c03f21acefd0d97f778050f9a268bec9d5a6d2
KS <91375185+ShouldKnowB@users.noreply.github.com>
2023-03-29 22:27:05 +0100

    Minor Changes

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.main')
 
 @section('content')
-    <br><br><br><br>
-
-    <h1>Summary of your order, {{ auth()->user()->name }}</h1>
-
-    @php
-        $subtotal = 0;
-    @endphp
-
-    @foreach ($products as $product)
-        <div>
-            <img src="/assets/images/productImages/{{ $product->product_image }}" alt="image" height="250" width="250">
-            {{ $product->product_name }}
-            £{{ $product->product_price }}
-            Quantity: {{ $product->quantity }}
-        </div>
+    <div class="checkout-page">
+        <h1>Summary of your order, {{ auth()->user()->name }}</h1>
 
         @php
-            $subtotal = $subtotal + $product->quantity * $product->product_price;
+            $subtotal = 0;
         @endphp
-    @endforeach
 
-    <div>
-        Total you have paid = £{{ $subtotal }}
+        @foreach ($products as $product)
+            <div>
+                <img src="/assets/images/productImages/{{ $product->product_image }}" alt="image" height="250"
+                    width="250">
+                {{ $product->product_name }}
+                £{{ $product->product_price }}
+                Quantity: {{ $product->quantity }}
+            </div>
+
+            @php
+                $subtotal = $subtotal + $product->quantity * $product->product_price;
+            @endphp
+        @endforeach
+
+        <div>
+            Total you have paid = £{{ $subtotal }}
+        </div>
+
+        <p>View your order details by clicking <a href="/previous">here</a></p>
     </div>
-
-    <p>View your order details by clicking <a href="/previous">here</a></p>
 @endsection
