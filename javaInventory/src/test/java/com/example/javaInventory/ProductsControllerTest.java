18de90563d170dd7ee0b3d8169adb2db739453d4
Hasnain Ali <hasnain7815@gmail.com>
2023-03-31 23:23:20 +0100

    Junit tests

--- javaInventory/src/test/java/com/example/javaInventory/ProductsControllerTest.java
+++ javaInventory/src/test/java/com/example/javaInventory/ProductsControllerTest.java
@@ -9,6 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -54,5 +56,19 @@ public class ProductsControllerTest {
 
         // Assert
         assertEquals("update-product", viewName);
+    }
+
+    @Test
+    void testDeleteProduct() {
+        // Arrange
+        Integer id = 1;
+        RedirectAttributes redirectAttributes = new RedirectAttributesModelMap();
+
+        // Act
+        String viewName = productsController.deleteProduct(Long.valueOf(id), redirectAttributes);
+
+        // Assert
+        assertEquals("redirect:/", viewName);
+        assertEquals("Product has been deleted successfully", redirectAttributes.getFlashAttributes().get("deleted"));
     }
 }
